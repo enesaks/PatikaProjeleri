f36f538505ec9851be61850195835045c287361b
Enes Akus <enes.aks29@hotmail.com>
2023-08-16 19:03:02 +0300

    Game,PLayer,Inventor ve Location classları oluşturldu.

---
+++ Java_102/MaveraOyunu/src/Inventory.java
@@ -0,0 +1,12 @@
+public class Inventory {
+
+    public boolean water;
+    public boolean food;
+    public boolean fireword;
+    public String weaponName;
+    public int weaponDemage;
+    public int armorDefence;
+
+
+
+}
---
+++ Java_102/MaveraOyunu/src/Location.java
@@ -0,0 +1,14 @@
+public abstract class Location {
+
+    public Player player;
+    public String name;
+
+
+    public Location(){
+
+    }
+
+    boolean onLocation;
+
+
+}
