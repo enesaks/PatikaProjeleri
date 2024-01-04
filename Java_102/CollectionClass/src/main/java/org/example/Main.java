e94f4aed8f77c25cac6e4f82b47998a3a17aa80e
Enes <98012557+enesaks@users.noreply.github.com>
2024-01-04 08:29:17 +0300

    Collectiion Class

---
+++ Java_102/CollectionClass/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        /* diilerde sayı artıkça istediğimiz değerleri bulmak zorlaşıyor.
+        int[] a = new int[5000];
+
+        for (int i = 0; i < a.length; i++){
+            if (a[i] == 123){
+                //Aranan değer
+            }
+        }*/
+
+        
+
+
+    }
+}
